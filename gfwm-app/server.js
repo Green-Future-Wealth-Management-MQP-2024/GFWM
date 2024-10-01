7e092ff5896aa0abcd5d11af65ba6dce1456930c
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2024-10-01 17:10:44 -0400

    prisma

--- gfwm-app/server.js
+++ gfwm-app/server.js
@@ -2,7 +2,42 @@ const express = require('express');
 const path = require('path');
 const vite = require('vite');
 const app = express();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const pythonShell = require('python-shell');
 
+// submit form data to the server
+app.post('/api/submit', async (req, res) => {
+  console.log(req.body);
+  res.json({ message: 'Data received!' });
+//post data to the database using prisma
+
+    try {
+      const newResponse = await prisma.SurveyResponse.create({
+        data: {
+          environmental: req.body.environmental,
+          social: req.body.social,
+          governance: req.body.governance,
+        },
+      });
+      res.json("data saved");
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'An error occurred!' });
+    }
+
+    //run python script
+    const scriptPath = 'path/to/script.py';
+    const args = req.body;
+    pythonShell.run(scriptPath, {args}, function (err, results) {
+      if (err) throw err;
+      console.log('results: %j', results);
+    });
+
+  });
+
+
+<<<<<<< Updated upstream
 // Middleware to parse incoming JSON requests
 app.use(express.json()); // This ensures req.body is populated
 
@@ -10,6 +45,8 @@ app.use(express.json()); // This ensures req.body is populated
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from the Express API!' });
 });
+=======
+>>>>>>> Stashed changes
 
 // Middleware to integrate Vite with Express in development
 if (process.env.NODE_ENV !== 'production') {
