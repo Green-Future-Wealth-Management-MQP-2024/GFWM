c9aff0054b4765d9ebc8eb1515c9e36d891f18bf
TEspelien <trajanespelien@gmail.com>
2024-10-03 14:43:32 -0400

    more progress on running python from the js server

--- gfwm-app/server.js
+++ gfwm-app/server.js
@@ -4,64 +4,10 @@ const vite = require("vite");
 const app = express();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const pythonShell = require("python-shell");
+const PythonShell = require("python-shell");
 
 // Middleware to parse incoming JSON requests
 app.use(express.json());
-
-// submit form data to the server
-app.post("/api/submit", async (req, res) => {
-  const clientResponses = req.body;
-
-  const data = {
-    environmentalScore:
-      (clientResponses.fossilFuels + clientResponses.environmental) / 2,
-    socialScore: (clientResponses.weapons + clientResponses.social) / 2,
-    governanceScore: clientResponses.governance,
-  };
-
-  //console.log(environmentalScore, socialScore, governanceScore);
-
-  //post data to the database using prisma
-  try {
-    const newResponse = await prisma.SurveyResponse.create({
-      data: data,
-    });
-    res.json("Questionnaire responses saved");
-  } catch (error) {
-    console.error("error with saving responses to database", error);
-    res
-      .status(500)
-      .json({ message: "Saving questionnaire responses to database failed" });
-  }
-
-  //run python script
-  const scriptPath = "path/to/script.py";
-  pythonShell.run(scriptPath, { data }, function (err, results) {
-    if (err) {
-      console.log("error with python script", err);
-      res.status(500).json("Python script failed");
-    }
-    console.log("python results: %j", results);
-
-    // Create an object with keys as "line1", "line2", etc.
-    const pythonResults = results.reduce((acc, line, index) => {
-      acc[`line${index + 1}`] = line;
-      return acc;
-    }, {});
-
-    //by this point everything went well!
-    res.status(200).json({
-      message: "Questionnaire responses saved and Python results provided!",
-      results: pythonResults,
-    });
-  });
-});
-
-// Serve API routes using Express
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from the Express API!" });
-});
 
 // Middleware to integrate Vite with Express in development
 if (process.env.NODE_ENV !== "production") {
@@ -85,8 +31,74 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.post("/submitForm", (req, res) => {
+// Serve API routes using Express
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from the Express API!" });
+});
+
+// submit form data to the server
+app.post("/submitForm", async (req, res) => {
+  console.log("received client responses");
+
   const clientResponses = req.body;
 
-  console.log(clientResponses);
+  const data = {
+    environmentalScore:
+      (clientResponses.fossilFuels + clientResponses.environment) / 2,
+    socialScore: (clientResponses.weapons + clientResponses.social) / 2,
+    governanceScore: clientResponses.governance,
+  };
+
+  console.log(data);
+
+  //post data to the database using prisma
+  try {
+    const newResponse = await prisma.SurveyResponse.create({
+      data: data,
+    });
+    res.json("Questionnaire responses saved");
+  } catch (error) {
+    console.error("error with saving responses to database", error);
+    res
+      .status(500)
+      .json({ message: "Saving questionnaire responses to database failed" });
+  }
+
+  //run python script
+
+  const scriptPath = "src/test_script.py";
+
+  let options = {
+    mode: "text",
+    pythonPath: "C:\Users\User\AppData\Local\Programs\Python\Python312\python.exe",
+    pythonOptions: ["-u"], // get print results in real-time
+    scriptPath: scriptPath,
+    args: [data.environmentalScore, data.socialScore, data.governanceScore],
+  };
+
+  PythonShell.run(scriptPath, options).then((pythonResults) => {
+    // results is an array consisting of messages collected during execution
+    if (err) {
+      console.log("error with python script", err);
+      res.status(500).json("Python script failed");
+    }
+
+    pythonResults = pythonResults[0];
+
+    console.log("python results: %j", pythonResults);
+
+    // Create an object with keys as "line1", "line2", etc.
+    /*
+    const pythonResults = results.reduce((acc, line, index) => {
+      acc[`line${index + 1}`] = line;
+      return acc;
+    }, {});
+    */
+
+    //by this point everything went well!
+    res.status(200).json({
+      message: "Questionnaire responses saved and Python results provided!",
+      results: pythonResults,
+    });
+  });
 });
