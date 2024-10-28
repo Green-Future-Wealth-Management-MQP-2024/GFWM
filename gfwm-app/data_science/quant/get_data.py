c0cd6e8c0def24cfc1b9fe1f0e969b9ff3866ea0
TEspelien <trajanespelien@gmail.com>
2024-10-28 18:22:48 -0400

    pulled out covariance calculation into separate script

--- gfwm-app/data_science/quant/get_data.py
+++ gfwm-app/data_science/quant/get_data.py
@@ -12,7 +12,6 @@ tickers = pd.read_csv("data_science/quant/constituents.csv")["Symbol"]
 tickers = tickers[~tickers.isin(["PEAK", "PXD", "WRK", "CDAY", "FLT", "BRK.B", "BF.B"])].reset_index(drop=True)
 
 all_data = pd.DataFrame()
-all_returns = {}
 
 # Loop through each ticker and fetch historical data
 for ticker in tickers:
@@ -42,18 +41,10 @@ for ticker in tickers:
     # drops the first 200 rows (largest moving average window)
     data = data.dropna(subset=['volume_100ma'])
 
-    all_returns[ticker] = data['log_return']
-
     # Append to the main DataFrame
     all_data = pd.concat([all_data, data])
 
 all_data.drop(columns=['squared_log_return'], inplace=True)
 
 # Save the combined DataFrame to CSV
-all_data.to_csv("sp500_weekly_data.csv", index=True)
-
-
-# Calculate the covariance matrix
-covariance_matrix = pd.DataFrame(all_returns).cov()
-
-covariance_matrix.to_csv("sp500_weekly_covariance_matrix.csv", index=True)
+all_data.to_csv("data_science/quant/sp500_weekly_data.csv", index=True)
