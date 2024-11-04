5be1a26d18d2929ab5a012492646a2161d059568
TEspelien <trajanespelien@gmail.com>
2024-11-04 15:54:00 -0500

    added eigenvalue check

--- gfwm-app/data_science/quant/portfolio_opt.m
+++ gfwm-app/data_science/quant/portfolio_opt.m
@@ -10,6 +10,11 @@ col_tickers = Omega{:, 1}; % First column as tickers
 % Convert the table to a numeric matrix, excluding the first column
 Omega = Omega{:, 2:end};
 
+% get eigenvalues before adjustments to check positive semidefinite
+[~, eigenvalues_matrix] = eig(Omega);
+eigenvalues_before = diag(eigenvalues_matrix);
+
+%{
 threshold = 0.0008; % Set an appropriate threshold based on your data scale
 [row, col] = find(abs(Omega) > threshold & ~eye(size(Omega)));
 pairs = unique(sort([row, col], 2), 'rows');
@@ -21,13 +26,17 @@ Omega(tickers_to_remove, :) = [];
 
 epsilon = 0.0003; % Small regularization term
 Omega = Omega + epsilon * eye(size(Omega));
+%}
+
+%alternative: find nearest positive semi definite
+
+% get eigenvalues after adjustments to confirm positive semidefinite
+[~, eigenvalues_matrix] = eig(Omega);
+eigenvalues_after = diag(eigenvalues_matrix);
 
 % Group by ticker and calculate the mean of the 'log_return' column
 mu = varfun(@mean, data, 'InputVariables', 'log_return', 'GroupingVariables', 'ticker');
-mu(tickers_to_remove,:) = [];
-
-% Display the table of ticker means
-disp(mu);
+%mu(tickers_to_remove,:) = [];
 
 p = Portfolio('AssetMean',mu.mean_log_return, 'AssetCovar',Omega,'lb', 0,'budget', 1);
 plotFrontier(p, 20);
