9f9a3c34627da1bdcb30156850a509ba6e176548
TEspelien <trajanespelien@gmail.com>
2024-11-07 15:35:50 -0500

    processed ycharts data into timeseries csv

--- gfwm-app/data_science/quant/get_data.py
+++ gfwm-app/data_science/quant/get_data.py
@@ -2,8 +2,13 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 
-tickers = pd.read_csv("data_science/Refinitiv ESG Final Data for Analysis.csv")["Symbol"].unique()
-tickers = pd.Series(tickers)
+esg_tickers = pd.read_csv("data_science/Refinitiv ESG Final Data for Analysis.csv")["Symbol"].unique()
+tickers = pd.read_csv("data_science/quant/SP500_ticker_metadata.csv")['ticker']
+
+#intersection of tickers in ESG data and the SP500 data we have
+#todo: only get the timeseries data for the ESG tickers we have
+tickers = pd.Series(list(set(esg_tickers) & set(tickers)))
+
 # remove PEAK, 
 # PXD (pioneer energy, aquired by exxon mobil), 
 # WRK (sidney australia listing)
@@ -12,43 +17,25 @@ tickers = pd.Series(tickers)
 
 # Filter out bad tickers using mask
 tickers = tickers[~tickers.isin(["PEAK", "PXD", "WRK", "CDAY", "FLT", "BRK.B", "BF.B"])]
-print(tickers)
 
-all_data = pd.DataFrame()
+timeseries_13_18 = pd.read_csv("data_science/quant/SP500_timeseries_1-1-13--12-31-18.csv")
+timeseries_19_24 = pd.read_csv("data_science/quant/SP500_timeseries_1-1-19--11-1-24.csv")
 
-# Loop through each ticker and fetch historical data
-for ticker in tickers:
-    # Fetch historical data for the ticker
-    print(ticker)
-    data = yf.download(ticker, interval = "1d", start="2000-01-01", end="2024-10-01")
+# remove columns (days) with no data, for example holidays
+timeseries_13_18 = timeseries_13_18.dropna(axis=1, how='all')
+timeseries_19_24 = timeseries_19_24.dropna(axis=1, how='all')
 
-    # Keep only the relevant columns and rename them
-    data = data[['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
-    data.columns = ['open', 'high', 'low', 'close', 'adjusted_close', 'volume']
+# transpose so each column turns in a timeseries for one ticker
+# also reverse order so dates increase from top to bottom
+timeseries_13_18 = timeseries_13_18.set_index('ticker').T.iloc[::-1]
+timeseries_13_18.columns.name = None
+timeseries_19_24 = timeseries_19_24.set_index('ticker').T.iloc[::-1]
+timeseries_19_24.columns.name = None
 
-    # add new columns
-    data.insert(0, "ticker", ticker)
+# Concatenate along the rows (axis=0)
+timeseries = pd.concat([timeseries_13_18, timeseries_19_24], axis=0)#.reset_index(drop = True)
 
-    # calculate two forms of return in new columns
-    data.insert(6, 'raw_return',
-                data['adjusted_close'] / data['adjusted_close'].shift(1))
-    data.insert(7, 'log_return', np.log(data['raw_return']))
-
-    # used to calculate volatility
-    data.insert(8, 'squared_log_return', data['log_return'] ** 2)
-
-    # calculate moving averages for volatility and volume
-    data.insert(9, 'volatility', np.sqrt(252) *
-                data['squared_log_return'].ewm(alpha=0.94, adjust=True).mean())
-    data.insert(11, 'volume_100ma', data['volume'].rolling(window=100).mean())
-
-    # drops the first 200 rows (largest moving average window)
-    data = data.dropna(subset=['volume_100ma'])
-
-    # Append to the main DataFrame
-    all_data = pd.concat([all_data, data])
-
-all_data.drop(columns=['squared_log_return'], inplace=True)
+#print(timeseries.shape)
 
 # Save the combined DataFrame to CSV
-all_data.to_csv("data_science/quant/sp500_daily_data_backup.csv", index=True)
+timeseries.to_csv("data_science/quant/sp500_timeseries_13-24.csv", index=True)
