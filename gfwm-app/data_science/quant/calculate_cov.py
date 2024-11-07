fixed covariance calculation using new data
Update calculate_cov.py

gfwm-app/data_science/quant/calculate_cov.py
@@ -1,25 +1,38 @@
 import pandas as pd
-from datetime import datetime, timedelta
+import numpy as np
 
-price_data = pd.read_csv("data_science/quant/sp500_daily_data.csv", parse_dates=["Date"])[['Date', 'ticker', 'log_return']]
+timeseries = pd.read_csv("data_science/quant/sp500_timeseries_13-24.csv")
 
-# only take into consideration last 10 years of price data
-# price_data = price_data[price_data['Date'] >= (datetime.now() - timedelta(days=10*365))]
+tickers = timeseries.columns[1:].values
 
-# remove these tickers which aren't even in the weekly data 
-#price_data = price_data[~price_data['ticker'].isin(["CDAY", "FLT"])]
+#print(tickers)
 
-# Since some tickers use different days within the same week, standardize week counts
-#min_date = price_data['Date'].min()
-#price_data['week'] = (price_data['Date'] - min_date).dt.days // 7
+# for every ticker, figure out the first day present in the dataset
+# then cov (A, B) will take only the data that is present for both A and B
+first_valid_indices = [timeseries[tickers[i]].first_valid_index() for i in range(len(tickers))]
 
+def covariance(x, y):
+    x_bar = np.mean(x)
+    y_bar = np.mean(y)
+    return np.sum((x - x_bar) * (y - y_bar)) / (len(x) - 1)
 
-# pivot to make 500 columns
-price_data = price_data.pivot(index = 'Date', columns='ticker', values='log_return')
+# empty cov matrix to be filled with nested for loop
+cov_matrix = pd.DataFrame(np.nan, index=tickers, columns=tickers)
 
-print(price_data.head(5))
+for i in range(len(tickers)):
+    for j in range(i, len(tickers)):  # Loop over upper triangle
 
-# pandas covariance function
-#covariance_matrix = price_data.cov()
+        # start at the later date after which both tickers have data
+        start = max(first_valid_indices[i], first_valid_indices[j])
+        
+        cov_matrix.iloc[i, j] = cov_matrix.iloc[j, i] = covariance(timeseries.iloc[start:, i+1], # add 1 since first column contains dates
+                                                                   timeseries.iloc[start:, j+1])
 
-#covariance_matrix.to_csv("data_science/quant/sp500_covariance_matrix.csv", index=True)
+eigenvalues = np.linalg.eigvals(cov_matrix)
+
+is_positive_semidefinite = np.all(eigenvalues >= 0)
+
+print("Cov matrix positive semidefinite?", is_positive_semidefinite)
+
+    
+cov_matrix.to_csv("data_science/quant/sp500_cov_matrix.csv", index=True)

gfwm-app/data_science/quant/get_data.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 
-esg_tickers = pd.read_csv("data_science/Refinitiv ESG Final Data for Analysis.csv")["Symbol"].unique()
-tickers = pd.read_csv("data_science/quant/SP500_ticker_metadata.csv")['ticker']
+esg_tickers = pd.read_csv("data_science/Refinitiv ESG Final Data for Analysis.csv")["Symbol"]
+ticker_metadata = pd.read_csv("data_science/quant/SP500_ticker_metadata.csv")
+
+tickers = ticker_metadata[ticker_metadata['days_since_ipo'] > 180]['ticker']
 
 #intersection of tickers in ESG data and the SP500 data we have
 #todo: only get the timeseries data for the ESG tickers we have
@@ -35,7 +37,10 @@ timeseries_19_24.columns.name = None
 # Concatenate along the rows (axis=0)
 timeseries = pd.concat([timeseries_13_18, timeseries_19_24], axis=0)#.reset_index(drop = True)
 
-#print(timeseries.shape)
+# replace each entry with the log return compared to the previous day
+# first row will turn into NaN so remove it
+timeseries = np.log(timeseries/timeseries.shift(1).iloc[1:])
 
+#print(timeseries.shape)
 # Save the combined DataFrame to CSV
 timeseries.to_csv("data_science/quant/sp500_timeseries_13-24.csv", index=True)