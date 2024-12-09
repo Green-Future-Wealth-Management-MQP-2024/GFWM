0185817455a8df854f86e31a88ee82935b1efce5
TEspelien <trajanespelien@gmail.com>
2024-12-09 14:21:47 -0500

    cleaned up form and landing page

--- gfwm-app/src/components/Landing.jsx
+++ gfwm-app/src/components/Landing.jsx
@@ -1,41 +1,25 @@
 import React, { useState } from "react";
 import RankingForm from "./RankingForm.jsx";
 
-import "./Landing.css";
-
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <header className="top-bar">
-      <h2 className="text-2xl font-bold">
-        Green Future Wealth Management ESG Questionnaire
-      </h2>
-
-    </header>
+        <h2 className="text-2xl font-bold">Green Future Wealth Management ESG Questionnaire</h2>
+      </header>
       <div className="body-content">
-        <h1>Welcome to Our ESG Questionnaire!</h1>
-        <p>Thank you for taking the time to answer these questions.</p>
-        <p>
-          Classify each factor below from not important to very important to your investments.
-          <br />
-          If you so choose, you will also be able to contact Green Future Wealth
-          Management to book a complimentary initial consultation today to
-          review your current portfolio.
-        </p>
-
         <RankingForm />
       </div>
-      <FooterBar />
-    </div>
-  );
-};
 
-const FooterBar = () => {
-  return (
-    <footer className="footer-bar">
-      <p>2024 Green Future Wealth Managagement MQP</p>
-      <p>Securities offered through Registered Representatives of Cambridge Investment Research, Inc., a broker dealer, member FINRA/SIPC. Advisory services offered through Cambridge Investment Research Advisors, Inc, a Registered Investment Adviser. Green Future Wealth Management and Cambridge are not affiliated. </p>
-    </footer>
+      <footer className="footer-bar">
+        <p>2024 Green Future Wealth Managagement MQP</p>
+        <p>
+          Securities offered through Registered Representatives of Cambridge Investment Research, Inc., a broker dealer,
+          member FINRA/SIPC. Advisory services offered through Cambridge Investment Research Advisors, Inc, a Registered
+          Investment Adviser. Green Future Wealth Management and Cambridge are not affiliated.{" "}
+        </p>
+      </footer>
+    </div>
   );
 };
 
