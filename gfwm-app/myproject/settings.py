b9086a387f575897464f3d914706b7534e828325
TEspelien <trajanespelien@gmail.com>
2025-02-02 16:47:21 -0500

    fixed links

--- gfwm-app/myproject/settings.py
+++ gfwm-app/myproject/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import Config, RepositoryEnv
-import dj_database_url
-
-config = Config(RepositoryEnv('.env.local'))
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
